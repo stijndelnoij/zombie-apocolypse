c6ac6ced7b44e42dfc90058a3b4d2c8089960eae
unknown <stijn.delnoij@student.uva.nl>
2018-05-09 15:28:24 +0200

    zitten paar bugs

--- testspace.py
+++ testspace.py
@@ -1,1 +1,9 @@
-print "cya"
+dick = {(1,3):1, (2,4):2, (3,5):3}
+for key in dick:
+    print key
+print (1,3) in dick
+L=['a','b','c','d']
+L.remove('b')
+print '%s' % L
+arr=[[2,3],[3,4],[4,5]]
+print '%d %d' % (len(arr), len(arr[0]))
--- zombies.py
+++ zombies.py
@@ -5,7 +5,48 @@ import math
 import numpy as np
 import time
 
-L_bonds = ['u', 'd', 'l', 'r']
+L_bonds = ['u', 'r', 'd', 'l']
+
+def removekey(d, key):
+    r = dict(d)
+    del r[key]
+    return r
+
+def opposite(direct):
+    if direct == 'u':
+        opp = 'd'
+    
+    elif direct == 'd':
+        opp = 'u'
+    
+    elif direct == 'r':
+        opp = 'l'
+    
+    elif direct== 'l':
+        opp = 'r'
+    
+    return opp
+
+def coordinates(direct, xy):
+    x = xy[0]
+    y = xy[1]
+    if direct == 'u':
+        x = x
+        y = y + 1
+
+    elif direct == 'd':
+        x = x
+        y = y - 1
+
+    elif direct == 'l':
+        x = x - 1
+        y = y
+
+    elif direct == 'r':
+        x = x + 1
+        y = y
+    
+    return x, y
 
 def randen(dick, xy, n):
     x0 = xy[0]
@@ -21,7 +62,7 @@ def randen(dick, xy, n):
     
     elif x0 == 0:
         dick[xy].remove('l')
-    
+
     return dick
     
 def zombies(n, p_thres):
@@ -29,12 +70,15 @@ def zombies(n, p_thres):
     start1 = time.time()
     x0 = random.randint(0, n - 1)
     y0 = random.randint(0, n - 1)
-    L_pos = [[0] * n for i in range(n)] 
+    L_pos = [[0] * (n + 1) for i in range(n + 1)] 
     L_pos[x0][y0] = 1
     xy = tuple([x0, y0])
     L_zomb = [xy]
     dick = {xy:L_bonds}
     dick = randen(dick, xy, n)
+    for i in range(n):
+        L_pos[i][n] = 3
+
     for i in range(n):
         for j in range(n):
             p = random.random()
@@ -48,23 +92,64 @@ def zombies(n, p_thres):
     return L_pos, L_zomb, dick
 
 def interactie(L_pos, L_zomb, dick, b, k):
-    for xy in L_zomb:
+    L_add, L_delete, L_remove = [], [], []
+    for xy in dick:
         x = xy[0]
         y = xy[1]
+        #print dick[xy]
         for direct in dick[xy]:
+            L_dir = ['u', 'd', 'l', 'r']
             p = random.random()
-            if b / float(b + k) > p:
-                if direct == 'u':
-                    L_pos[x][y+1] = 2
-                    dick[(x,y)].remove('u')
-                    dick[(x,y+1)]=['d','l','r']
-                elif direct == 'd':
-                    L_pos[x][y-1] = 2
-                    dick[(x,y)].remove('d')
-                    dick[()]
+            xnew, ynew = coordinates(direct, xy)
+            #print direct
+            if b / float(b + k) > p and xnew < len(L_pos[0]) and ynew < len(L_pos):
+                L_pos[xnew][ynew] = 1
+                L_remove.append((xy, direct))
+                L_dir.remove(opposite(direct))
+                L_add.append((xnew, ynew, L_dir))
+            
+            elif k / float(b + k) > p:
+                L_pos[x][y] = 3
+                L_delete.append(xy)
 
-L_pos, L_zomb, dick = zombies(200, 0.5)
+    for newzomb in L_add:
+        xnew, ynew, L_dir = newzomb
+        dick[(xnew, ynew)] = L_dir
+
+    for xy in L_delete:
+        dick = removekey(dick, xy)
+        for direct in L_bonds:
+            xnew, ynew = coordinates(direct, xy)
+            if (xnew, ynew) in dick:
+                L = dick[(xnew, ynew)]
+
+                try:
+                    L.remove(opposite(direct))
+                    dick[(xnew, ynew)] = L
+
+                except:
+                    b = 1
+
+                
+
+    return L_pos, L_zomb, dick
+
+n = 20
+L_pos, L_zomb, dick = zombies(n, 1.)
+print "%s" % len(L_pos[1])
 data = np.array(L_pos)
-cmap = colors.ListedColormap(['black','red','green'])
+plt.figure(1)
+plt.xlim(-0.5, n - 0.5)
+plt.ylim(-0.5, n - 0.5)
+cmap = colors.ListedColormap(['white', 'red', 'green', 'black'])
+plt.imshow(data, interpolation='none', cmap=cmap, origin = 'lower')
+plt.figure(2)
+for i in range(5):
+    L_pos, L_zomb, dick = interactie(L_pos, L_zomb, dick, 0.1, 1)
+
+data = np.array(L_pos)
+plt.xlim(-0.5, n - 0.5)
+plt.ylim(-0.5, n - 0.5)
+cmap = colors.ListedColormap(['black','red','green', 'white'])
 plt.imshow(data, interpolation='none', cmap=cmap, origin = 'lower')
 plt.show()
